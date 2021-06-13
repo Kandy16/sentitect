53ca1d22ea945a054454b13da3e06ccb5b151faf
Kandy16 <kandhasamy.rajasekaran@gmail.com>
2021-06-13 03:31:54 +0200

    Code added for sample classification training and prediction, sample hyper-parameter tuning

---
+++ sample.py
@@ -0,0 +1,30 @@
+"""
+Code samples inspired from 
+
+https://machinelearningmastery.com/light-gradient-boosted-machine-lightgbm-ensemble/
+
+"""
+
+
+# evaluate lightgbm algorithm for classification
+import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from lightgbm import LGBMClassifier
+# define dataset
+X, y = make_classification(n_samples=1000, n_features=20, n_informative=15, n_redundant=5, random_state=7)
+# define the model
+model = LGBMClassifier()
+# evaluate the model
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+n_scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+# report performance
+print('Accuracy: %.3f (%.3f)' % (np.mean(n_scores), np.std(n_scores)))
+
+# fit the model on the whole dataset
+model.fit(X, y)
+# make a single prediction
+row = [0.2929949,-4.21223056,-1.288332,-2.17849815,-0.64527665,2.58097719,0.28422388,-7.1827928,-1.91211104,2.73729512,0.81395695,3.96973717,-2.66939799,3.34692332,4.19791821,0.99990998,-0.30201875,-4.43170633,-2.82646737,0.44916808]
+yhat = model.predict([row])
+print('Predicted Class: %d' % yhat[0])
